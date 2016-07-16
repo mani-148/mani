4fb99e2084262144f9c6639f85263c51b5b5dac1
mani-148 <balasubramanian.204@gmail.com>
2016-07-16 16:02:57 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student2;
+
+/**
+ *
+ * @author Student
+ */
+public class factorial {
+    public static void main(String[] args) {
+        
+    
+    int i,fact=1;  
+  int number=12;//It is the number to calculate factorial    
+  for(i=1;i<=number;++i)
+  {    
+      fact=fact*i;    
+  }    
+  System.out.println("Factorial of "+number+" is: "+fact);    
+ }  
+}  
+    
+
